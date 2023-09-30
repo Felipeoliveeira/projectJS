1bfda4ae63c43f717db3af1466122d6f02d73625
Felipe José Santos de Oliveira <felipeoliveira.aju79@gmail.com>
2023-09-30 20:37:10 -0300

    Criando o JS do jogo do 'numero de acerto'

---
+++ logica-js/app.js
@@ -0,0 +1,10 @@
+alert('Boas vindas ao jogo do número secreto!');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um n´meroentre 1 e 10');
+
+if (chute == numeroSecreto) {
+    alert('Isso aí! Você descobriu o número secreto (5)')
+}
+else{
+    alert('errou')
+}
