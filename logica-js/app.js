aa7576db3460c6be84fc0ff7138974c2d4971670
Felipe José Santos de Oliveira <felipeoliveira.aju79@gmail.com>
2023-10-02 20:41:33 -0300

    else alert('errou')

--- logica-js/app.js
+++ logica-js/app.js
@@ -1,10 +1,10 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5;
-let chute = prompt('Escolha um n´meroentre 1 e 10');
+let chute = prompt('Escolha um número de 1 e 10');
 
 if (chute == numeroSecreto) {
-    alert('Isso aí! Você descobriu o número secreto (5)')
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
 }
 else{
-    alert('errou')
+    alert(`Você errou, o número certo é ${numeroSecreto} e você escolheu ${chute}!`)
 }
