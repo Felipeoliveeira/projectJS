de5638a7bc78ad23388f1df44677dab5eeebdc51
Felipe José Santos de Oliveira <felipeoliveira.aju79@gmail.com>
2023-10-03 20:57:16 -0300

    While(){}

--- logica-js/app.js
+++ logica-js/app.js
@@ -1,10 +1,21 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5;
-let chute = prompt('Escolha um número de 1 e 10');
+let chute;
+let tentativas = 1;
 
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
-}
-else{
-    alert(`Você errou, o número certo é ${numeroSecreto} e você escolheu ${chute}!`)
+while (chute != numeroSecreto){
+    chute = prompt('Escolha um número de 1 e 10');
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+    }
+    else{
+        if (numeroSecreto > chute){
+            alert(`Você errou, o número secreto é maior que ${chute}`);
+            prompt('Escolha um número de 1 e 10');
+        }else{
+            alert(`Você errou, o número secreto é menor que ${chute}`);
+            prompt('Escolha um número de 1 e 10');
+        }
+    }
+    tentativas++
 }
