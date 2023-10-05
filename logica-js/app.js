a333e3dd8ff0ffd4f81ec33d209621a2fb2ff287
Felipe José Santos de Oliveira <felipeoliveira.aju79@gmail.com>
2023-10-04 21:28:21 -0300

    parseInt(Math.random( )* 10 + 1);

--- logica-js/app.js
+++ logica-js/app.js
@@ -1,21 +1,26 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 1000);
 let chute;
 let tentativas = 1;
 
 while (chute != numeroSecreto){
-    chute = prompt('Escolha um número de 1 e 10');
+    chute = prompt('Escolha um número de 1 e 1000');
     if (chute == numeroSecreto) {
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+        break;
     }
     else{
         if (numeroSecreto > chute){
             alert(`Você errou, o número secreto é maior que ${chute}`);
             prompt('Escolha um número de 1 e 10');
+
         }else{
             alert(`Você errou, o número secreto é menor que ${chute}`);
             prompt('Escolha um número de 1 e 10');
+
         }
     }
     tentativas++
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
