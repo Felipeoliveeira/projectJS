4a8c9eb7c355ea98504bc07e19512a90f331188c
Felipe José Santos de Oliveira <felipeoliveira.aju79@gmail.com>
2023-10-11 20:42:50 -0300

    contando as tentativas

--- curso2-js/app.js
+++ curso2-js/app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = contadorNumeroSecreto();
+let tentativas = 1;
 console.log(numeroSecreto);
 
 function exibirTextoNaTela(tag,texto) {
@@ -11,12 +12,21 @@ exibirTextoNaTela('p', 'Escolha um número, de 1 a 10:');
 
 function verificarChute(){
     let chute = document.querySelector('.container__input').value;
-    // if ( chute == numeroSecreto) {
-    //     exibirTextoNaTela('p', 'Parabéns, você acertou o número secreto, vamos novamente!');
-    // }else{
-    //     exibirTextoNaTela('p', 'Errrrrrou!!!');
-    // }
-    console.log(chute == numeroSecreto);
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    let mensagemAcertou = `Parabéns, você acertou o número secreto com ${tentativas} ${palavraTentativa}.`
+        if ( chute == numeroSecreto) {
+            exibirTextoNaTela('p', mensagemAcertou);
+            exibirTextoNaTela('h1', 'Acertou!');
+        }else{
+            if( chute > numeroSecreto){
+                exibirTextoNaTela('p', `O número secreto é menor que ${chute}`);
+            }else{
+                if ( chute < numeroSecreto){
+                    exibirTextoNaTela('p', `O número secreto é maior que ${chute}`)
+                }
+            }
+        }
+    tentativas++;
 }
 
 function contadorNumeroSecreto() {
