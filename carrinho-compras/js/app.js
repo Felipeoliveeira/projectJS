f79ba369ee8c560c4734da265a52dd05582bfef7
Felipe José Santos de Oliveira <felipeoliveira.aju79@gmail.com>
2023-10-27 11:57:59 -0300

    Finalizando carrinho de compras

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,24 +1,27 @@
+let valorTotal = 0;
+limpar();
 function adicionar() {
     // recuperar nome e quantidade e valor do produto
     let produtoEscolhido = document.getElementById('produto').value;
     let quantidadeProduto = document.querySelector('.quantidade-input').value;
     let nomeProduto = produtoEscolhido.split('-')[0];
     let valorProduto = produtoEscolhido.split('R$')[1];
-    console.log(nomeProduto);
-    console.log(valorProduto);
     // calcular o preco, subtotal
-    let valorSubtotal = quantidadeProduto * valorProduto;
-    console.log(valorSubtotal);
-    // adicionar carrinho
+    let preco = quantidadeProduto * valorProduto;
     let listaCarrinho = document.getElementById('lista-produtos');
+    // adicionar carrinho
     listaCarrinho.innerHTML = listaCarrinho.innerHTML + `<section class="carrinho__produtos__produto">
     <span class="texto-azul">${quantidadeProduto}x</span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span>
     </section>`;
     // valor total
-    let valorTotal = document.getElementById('valor-total');
-    valorTotal.innerHTML = valorSubtotal + 1400;
+    valorTotal = valorTotal + preco;
+    let CampoValorTotal = document.getElementById('valor-total');
+    CampoValorTotal.innerHTML = `R$ ${valorTotal}`;
+    document.querySelector('.quantidade-input').value = 0;
 }
 
 function limpar(){
-    
-}
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').innerHTML = `R$ ${valorTotal}`;
+    valorTotal = 0;
+} 
