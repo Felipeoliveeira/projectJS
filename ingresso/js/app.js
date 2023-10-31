18b50b6c816302569e246435e25f14606bc5daf9
Felipe José Santos de Oliveira <felipeoliveira.aju79@gmail.com>
2023-10-31 19:04:30 -0300

    projeto ingressos finalizado

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,13 +1,48 @@
 function comprar() {
-    let tipo = document.getElementById('tipo-ingresso').value;
-    let qtd = document.getElementById('qtd').value;
-    let qtdDisponivel = document.getElementById(`qtd-${tipo}`).textContent;
-    let decremento = qtdDisponivel - qtd;
-    if ( qtdDisponivel > 0 & qtd < qtdDisponivel) {
-        document.getElementById(`qtd-${tipo}`).innerHTML = decremento;
-        alert('Compra feita com sucesso')
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = parseInt(document.getElementById('qtd').value);
+    
+    if ( tipo.value == 'pista') {
+        comprarPista(qtd);
     } else {
-        alert('Tipo ou quantidade indisponível')
-        console.log(qtdDisponivel)
+        if( ( tipo.value == 'superior')){
+            comprarSuperior(qtd);
+        } else {
+            comprarInferior(qtd);
+            
+        }
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if ( qtdInferior < qtd) {
+        alert('Quantidade ou venda indisponivel');
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtdSuperior < qtd){
+        alert('Quantidade ou venda indisponivel');
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if ( qtdPista < qtd ) {
+        alert('Quantidade ou venda indisponivel')
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!')
     }
 }
