44c10a27f23d3ab4ca98f9fae09d22129938f34c
Felipe José Santos de Oliveira <felipeoliveira.aju79@gmail.com>
2023-11-08 20:14:40 -0300

    manipulando cor de fundo com o addEventListener( );

--- projectFokus/js/app.js
+++ projectFokus/js/app.js
@@ -1,11 +1,19 @@
-let html = document.querySelector('html');
-let focoBt = document.querySelector('app__card-button--foco');
-let curtoBt = document.querySelector('app__card-button--curto');
-let longoBt = document.querySelector('app__card-button--longo');
+const html = document.querySelector('html');
+const focoBt = document.querySelector('.app__card-button--foco');
+const curtoBt = document.querySelector('.app__card-button--curto');
+const longoBt = document.querySelector('.app__card-button--longo');
+const imagem = document.querySelector('.app__image');
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
+    imagem.setAttribute('src', './imagens/foco.png')
 })
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
+    html.setAttribute('data-contexto', 'descanso-curto');
+    imagem.setAttribute('src', './imagens/descanso-curto.png')
+})
+
+longoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo')
+    imagem.setAttribute('src', './imagens/descanso-longo.png')
 })
